b9fcbe12433a7a09dafb7fc6103b9cb91cfd2b2b
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2016-07-10 13:43:15 -0400

    create monster

--- app/controllers/monster_controller.rb
+++ app/controllers/monster_controller.rb
@@ -1,5 +1,11 @@
 class MonsterController < ApplicationController
   def index
-    render json: {}
+    render json: Monster.order('created_at desc').all.to_json
+  end
+
+  def create
+    # check if name is a real pokemon
+    Monster.create(params[:monster])
+    render :nothing => true, :status => 200, :content_type => 'text/html'
   end
 end
