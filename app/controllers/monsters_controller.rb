58710412208f9b08f74e6920be25951e39892e58
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2016-07-10 14:01:16 -0400

    fixed routes, validations

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -2,7 +2,7 @@ class MonstersController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def index
-    render json: Monster.order('created_at desc').all.to_json
+    render json: Monster.where.not(lat: nil).order('created_at desc').all.to_json
   end
 
   def create
--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,2 +1,3 @@
 class Monster < ActiveRecord::Base
+  validates :name, :lat, :lng, presence: true
 end
