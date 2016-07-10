66639c02a1e76d4b9fcfc28aa48dfe658a83a7d6
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2016-07-10 13:36:21 -0400

    add monsters models

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160710173537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "monsters", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "lat",        precision: 10, scale: 6
+    t.decimal  "lng",        precision: 10, scale: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
