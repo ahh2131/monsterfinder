b5d16112e3738e47cbb343dde820ffbfb2857775
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2016-07-16 14:12:00 -0400

    v2

---
+++ app/controllers/v2/monsters_controller.rb
@@ -0,0 +1,65 @@
+class V2::MonstersController < V2::BaseController
+
+
+
+
+  DISTANCE = 10
+
+  skip_before_filter :verify_authenticity_token
+
+  def index
+    #render json: Monster.where.not(lat: nil).order('created_at desc')
+    #  .where(created_at: 3.hours.ago..Time.now).all.to_json
+    #expires_in 5.minutes, :public => true
+    m = []
+    if coordinates_exist?
+      m = Monster
+          .with_associations
+          .recent(params[:recent] == 'true')
+          .highly_rated(params[:rated] == 'true')
+          .near(coordinates, DISTANCE)
+          .all
+    end
+    render json: MonsterBuilder.new(m).render.as_json
+  end
+
+  def create
+    # check if name is a real pokemon
+    # this can be delayed job
+    if params.key?(:uuid)
+      m = Monster.create(monster_params)
+      # if user changes name, this doesnt work
+      u = User.first_or_create!(uuid: params[:uuid], name: params[:name])
+      Activity.create(user: u, monster: m, activity_type: "spot")
+    end
+    render :nothing => true, :status => 200, :content_type => 'text/html'
+  end
+
+  def search
+    monster = params[:monster].titleize
+    if Monster::MONSTERS.include?(monster)
+      m = if params[:recent] == 'true'
+            Monster.recent.near(coordinates, DISTANCE).where(name: monster).all
+          else
+            Monster.near(coordinates, DISTANCE).where(name: monster).all
+          end
+      render json: MonsterBuilder.new(m).render.as_json
+    else
+      render json: []
+    end
+  end
+
+  def coordinates
+    [params[:lat], params[:lng]]
+  end
+
+  def coordinates_exist?
+    params[:lat].to_f != 0.0 && params[:lng].to_f != 0.0
+  end
+
+  private
+
+  def monster_params
+    params.require(:monster).permit(:monster, :name, :lat, :lng)
+  end
+end
---
+++ app/controllers/v2/users_controller.rb
@@ -0,0 +1,42 @@
+class V2::UsersController < V2::BaseController
+
+  skip_before_filter :verify_authenticity_token
+
+  def show
+  end
+
+  def vote
+    # needs uuid to make sure hasnt already voted
+    u = User.where(uuid: params[:uuid]).first
+    m = Monster.find(params[:monster_id])
+    a = Activity.where(
+      user: u,
+      monster: m,
+      activity_type: [1, 2]
+    ).first
+    if a.nil?
+      Activity.create(
+        user: u,
+        monster: m,
+        activity_type: which_vote
+      )
+    end
+
+    render :nothing => true, :status => 200, :content_type => 'text/html'
+  end
+
+  def which_vote
+    params[:vote] == 'up' ? 'upvote' : 'downvote'
+  end
+
+  def create
+    User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :uuid)
+  end
+
+end
