452aa14f0a1dc64c3037180d2ff79c55823ebed9
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2016-07-15 23:50:00 -0400

    test

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -18,6 +18,7 @@ class MonstersController < ApplicationController
             Monster.with_associations.near(coordinates, DISTANCE).all
           end
     end
+    m  = Monster.all
     render json: MonsterBuilder.new(m).render
   end
 
