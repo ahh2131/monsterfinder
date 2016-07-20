787f0cb9f89291fde869efc8628968ad5cc97191
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2016-07-20 00:08:41 -0400

    chatroom number change for screenshots

--- app/controllers/v3/chatrooms_controller.rb
+++ app/controllers/v3/chatrooms_controller.rb
@@ -1,6 +1,6 @@
 class V3::ChatroomsController < V3::BaseController
 
   def get_chatroom
-    render json: ChatroomBuilder.new(12).render.to_json
+    render json: ChatroomBuilder.new(2).render.to_json
   end
 end
