b2bb57d432a3ddc57bd7c2b7204a6bdfc666ea67
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2016-07-20 08:38:49 -0400

    rate limiting

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -7,8 +7,13 @@ class Rack::Attack
   #   "rack::attack:#{Time.now.to_i/1.second}:req/ip:#{req.ip}"
   #
   # If falsy, the cache key is neither incremented nor checked.
-
+    Rails.cache.write("block #{req.ip}", true, expires_in: 1.days)
 
     req.ip
+  end
+
+  Rack::Attack.blacklist('block <ip>') do |req|
+    # if variable `block <ip>` exists in cache store, then we'll block the request
+    Rails.cache.fetch("block #{req.ip}").present?
   end
 end
