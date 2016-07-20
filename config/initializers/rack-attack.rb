cdefa7eec36b70eb9f311fec2a3cd979ee668e12
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2016-07-20 08:33:22 -0400

    blacklist

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,12 +1,16 @@
 class Rack::Attack
   # your custom configuration...
 
-  Rack::Attack.throttle('req/ip', :limit => 1, :period => 1.second) do |req|
+  Rack::Attack.throttle('req/ip', :limit => 5, :period => 1.second) do |req|
   # If the return value is truthy, the cache key for the return value
   # is incremented and compared with the limit. In this case:
   #   "rack::attack:#{Time.now.to_i/1.second}:req/ip:#{req.ip}"
   #
   # If falsy, the cache key is neither incremented nor checked.
+    Rack::Attack.blocklist('block ip') do |req|
+      # Requests are blocked if the return value is truthy
+      req.ip
+    end
 
     req.ip
   end
