24454fff490f7f1d97ddc2ed45108ec8cb9f1c49
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2016-07-20 09:11:10 -0400

    more strict limiting

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,13 +1,13 @@
 class Rack::Attack
   # your custom configuration...
 
-  Rack::Attack.throttle('req/ip', :limit => 5, :period => 1.second) do |req|
+  Rack::Attack.throttle('req/ip', :limit => 100, :period => 10.minutes) do |req|
   # If the return value is truthy, the cache key for the return value
   # is incremented and compared with the limit. In this case:
   #   "rack::attack:#{Time.now.to_i/1.second}:req/ip:#{req.ip}"
   #
   # If falsy, the cache key is neither incremented nor checked.
-    Rails.cache.write("block #{req.ip}", true, expires_in: 1.days)
+    Rails.cache.write("block #{req.ip}", true, expires_in: 5.days)
 
     req.ip
   end
