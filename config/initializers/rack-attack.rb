f956cd6c063c2efe9340cdcc7e43193b0a3d4ff4
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2016-07-23 07:08:12 -0400

    rack ttack

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,16 +1,16 @@
 class Rack::Attack
   # your custom configuration...
 
-  Rack::Attack.throttle('req/ip', :limit => 100, :period => 10.minutes) do |req|
+#  Rack::Attack.throttle('req/ip', :limit => 100, :period => 10.minutes) do |req|
   # If the return value is truthy, the cache key for the return value
   # is incremented and compared with the limit. In this case:
   #   "rack::attack:#{Time.now.to_i/1.second}:req/ip:#{req.ip}"
   #
   # If falsy, the cache key is neither incremented nor checked.
-    Rails.cache.write("block #{req.ip}", true, expires_in: 5.days)
+#    Rails.cache.write("block #{req.ip}", true, expires_in: 5.days)
 
-    req.ip
-  end
+#    req.ip
+#  end
 
   #Rack::Attack.blacklist('block <ip>') do |req|
     # if variable `block <ip>` exists in cache store, then we'll block the request
